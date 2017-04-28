73ca46f55de5424c1311f47c984992017a9240f2
Roy <cf020031308@163.com>
2017-04-28 18:55:40 +0800

    Fix a bug on calculating working hours

--- scripts/daily_report.py
+++ scripts/daily_report.py
@@ -22,10 +22,12 @@ def daily_report(date=None, scale_to=8, hours_unit=0.25):
     h = 'active_duration'
     # scale to 8 hours :)
     if scale_to:
-        scale_to -= sum(
-            task[h] for task in tasks
-            if task['project'] not in TIMESHEET_MAP)
-        ratio = scale_to * 3600.0 / sum(
+        scale_to *= 3600.0
+        ratio = (
+            scale_to - sum(
+                task[h] for task in tasks
+                if task['project'] not in TIMESHEET_MAP)
+        ) / sum(
             task[h] for task in tasks
             if task['project'] in TIMESHEET_MAP)
         if ratio > 1:
@@ -34,11 +36,10 @@ def daily_report(date=None, scale_to=8, hours_unit=0.25):
                     task[h] *= ratio
 
     # put to timesheet
+    def key(task):
+        return TIMESHEET_MAP.get(task['project'], task['project'])
     logs = []
-    for project, ts in itertools.groupby(
-            tasks,
-            key=lambda task: TIMESHEET_MAP.get(
-                task['project'], task['project'])):
+    for project, ts in itertools.groupby(sorted(tasks, key=key), key=key):
         ts = list(ts)
         logs.append((
             project,
