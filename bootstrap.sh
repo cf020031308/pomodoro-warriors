01cfb068baac214d810de7d6d3ac44e76ea6527f
Roy <cf020031308@163.com>
2017-10-13 12:09:12 +0800

    Using Context

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@ mkdir $HOME/.task
 ln -sf $HOME/OneDrive/task/completed.data $HOME/.task/completed.data
 ln -sf $HOME/OneDrive/task/pending.data $HOME/.task/pending.data
 ln -sf $PWD/hooks $HOME/.task/hooks
-ln -sf $PWD/taskrc $HOME/.taskrc
+mv $HOME/.taskrc $HOME/.taskrc.bak
+echo "include $PWD/taskrc" > $HOME/.taskrc
