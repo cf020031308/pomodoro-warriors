b44cb52136d0da4e40ab8390fecace2c47a51d21
Roy <cf020031308@163.com>
2017-11-08 16:26:29 +0800

    BugFix

--- taskwarrior/hooks/on-modify.py
+++ taskwarrior/hooks/on-modify.py
@@ -10,10 +10,11 @@ def main():
     inputs = utils.format_inputs()
     task = inputs['task']
     ret = [json.dumps(task)]
-    if 'estimate' in task:
-        ret.append(
-            'Estimate Duration: %s' % utils.parse_duration(task['estimate']))
     if 'end' in task and 'end' not in inputs['prior']:
+        if 'estimate' in task:
+            ret.append(
+                'Estimate Duration: %s' %
+                utils.parse_duration(task['estimate']))
         ret.append(
             'Total Duration: %s' % commands.getoutput(
                 'timew duration "%(uuid)s" from %(entry)s to %(end)s' % task))
