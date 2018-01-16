28b00f8d04bf34d548506cb2d2cd3803bbff5fe8
Roy <cf020031308@163.com>
2018-01-16 12:09:38 +0800

    BugFix

--- timewarrior/extensions/last.sh
+++ timewarrior/extensions/last.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-timew get dom.tracked.1.json | jq '.tags[0]' | xargs -I{} task uuid:{} info
+timew get dom.tracked.1.json | jq '.tags | map(select(length == 36))[0]' | xargs -I{} task uuid:{} info
