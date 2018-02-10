08d7b52f7be3eecfa1bfa84d3bad8a176df29a2d
Roy <cf020031308@163.com>
2018-02-10 17:34:59 +0800

    Remove the use of jq

--- timewarrior/extensions/last.sh
+++ timewarrior/extensions/last.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-timew get dom.tracked.1.json | jq '.tags | map(select(length == 36))[0]' | xargs -I{} task uuid:{} info
+
+timew get dom.tracked.1.json | python -c "
+import json
+import sys
+for tag in json.load(sys.stdin)['tags']:
+    if len(tag) == 36 and tag.count('-') == 4:
+        print(tag)
+        break
+" | xargs -I{} task uuid:{} info
