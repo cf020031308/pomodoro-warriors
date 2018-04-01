1ef1667e2c2fd6fd44188961c6f868195350ce7d
Roy <cf020031308@163.com>
2018-04-01 16:31:56 +0800

    fix configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,10 @@ context.inbox=-PROJECT
 context.work=project:ks +PROJECT
 context.home=project.not:ks +PROJECT" > ~/.taskrc
 
-if [ $1 ] then
-    mkdir -p $1
+if [ $1 ]; then
+    mkdir -p $1/task/time
+    touch $1/task/time{completed.data,pending.data}
     ln -sf $1/task/completed.data $HOME/.task/completed.data
     ln -sf $1/task/pending.data $HOME/.task/pending.data
-    ln -sf $1/time $HOME/.timewarrior/data
+    ln -sf $1/task/time $HOME/.timewarrior/data
 fi
